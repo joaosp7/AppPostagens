82e4c48be50ff94220da273742f3bc6b40edfca3
joaosp7 <joaosp7@gmail.com>
2025-01-26 12:25:07 -0300

    create project

---
+++ src/db/schema.ts
@@ -0,0 +1,7 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const userTable = pgTable('users', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 150 }).notNull(),
+  email: varchar({ length: 150} ).notNull().unique(),
+});
