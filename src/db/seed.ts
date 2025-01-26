d2bdf9a145bf68c94743bcd9514e0b0aab2ed74e
João Lucas Rodrigues <joaosp7@gmail.com>
2025-01-26 15:04:21 -0300

    tsx set up and seed for db

---
+++ src/db/seed.ts
@@ -0,0 +1,17 @@
+import { db } from ".";
+import { CreateUserDto } from "../users/dto/create-user.dto";
+import { userTable } from "./schema";
+
+async function seed() {
+  const user:CreateUserDto = {
+    email: 'sonic@gmail.com',
+    name: 'sonic',
+  };
+
+  await db.insert(userTable).values(user);
+
+}
+
+seed().then(()=>{
+  console.log('User inserted on database!');
+})
